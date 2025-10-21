92e546e2a1d6b5cdad256ba728d8f1db62c3af2b
yuvraj gupta <yuvigupta980714@gmail.com>
2025-10-21 20:27:26 +0530

    Add files via upload

---
+++ import math.py
@@ -0,0 +1,5 @@
+import math
+x=float(input(" enter your number : "))
+print(f"square root of {x} will be :{math.sqrt(x)}")
+print(f"logorithm of {x} will be : {math.log(x)}")
+print(f"the sine of {x} will be : {math.sin(x)}")
